837aaa022c18fda9b6086c90f0d60b500dabc468
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-07-21 12:14:34 -0400

    update

--- windows/admin.ps1
+++ windows/admin.ps1
@@ -14,3 +14,11 @@ Get-ChildItem $root -Recurse -File | ForEach-Object {
     New-Item -ItemType Directory -Path (Split-Path $dest) -Force | Out-Null
     Copy-Item $_.FullName $dest -Force
 }
+
+$release = Invoke-RestMethod -Uri "https://api.github.com/repos/rustdesk/rustdesk/releases/latest"
+$msi = $release.assets | Where-Object { $_.name -like "*x86_64.msi" } | Select-Object -First 1
+$file = Join-Path $env:TEMP $msi.name
+Invoke-WebRequest -Uri $msi.browser_download_url -OutFile $file
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$file`" /qn /norestart" -Wait
+Remove-Item $file -Force
+# https://support.google.com/android/answer/12623953
