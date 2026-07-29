d06f1e494e3efd75ff3b5c286812f889c3478fca
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-07-29 17:17:15 -0300

    Update admin.ps1

--- windows/admin.ps1
+++ windows/admin.ps1
@@ -7,11 +7,10 @@ powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 $guid = (powercfg /list | Select-String "Ultimate Performance").ToString().Split()[3]
 powercfg /setactive $guid
 
-$root = Join-Path $PSScriptRoot "C:"
+$root = Join-Path $PSScriptRoot "C"
 Get-ChildItem $root -Recurse -File | ForEach-Object {
     $relative = $_.FullName.Substring($root.Length).TrimStart("\")
-    $dest = "C:\" + $relative
-    $dest = $dest.Replace("{USER}", $env:USERNAME)
+    $dest = "C:\" + $relative.Replace("\Users\user\", "\Users\$env:USERNAME\")
     New-Item -ItemType Directory -Path (Split-Path $dest) -Force | Out-Null
     Copy-Item $_.FullName $dest -Force
 }
