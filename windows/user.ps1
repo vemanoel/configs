3862ed7ebf40564d3901b5af090ebb660387e776
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-08-14 10:28:26 -0400

    Update user.ps1

--- windows/user.ps1
+++ windows/user.ps1
@@ -9,6 +9,10 @@ winget install --id RARLab.WinRAR --exact --silent --accept-package-agreements -
 winget install --id Anki.Anki --exact --silent --accept-package-agreements --accept-source-agreements
 winget install --id Telegram.TelegramDesktop --exact --silent --accept-package-agreements --accept-source-agreements
 
+$path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search"
+New-Item -Path $path -Force | Out-Null
+New-ItemProperty -Path $path -Name SearchboxTaskbarMode -Type DWord -Value 0 -Force | Out-Null
+
 $path = "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings"
 New-Item -Path $path -Force | Out-Null
 New-ItemProperty -Path $path -Name IsMSACloudSearchEnabled -PropertyType DWord -Value 0 -Force | Out-Null
