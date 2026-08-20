739f195d9eb7d214109e52cb55e1bd17d57fe391
Victor Emanoel <victoremanoelmoraesamaral@gmail.com>
2026-08-20 12:28:40 -0300

    Update admin.ps1

--- windows/admin.ps1
+++ windows/admin.ps1
@@ -77,14 +77,13 @@ Start-Process "$env:windir\Resources\Themes\dark.theme"
 
 Remove-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Accessibility" -Recurse -Force
 
-# winget install --id RaveInc.Rave --exact --silent --accept-package-agreements --accept-source-agreements --ignore-security-hash
-# winget install --id BlueStack.BlueStacks --exact --silent --accept-package-agreements --accept-source-agreements
-# winget install --id Discord.Discord --exact --silent --accept-package-agreements --accept-source-agreements
-winget install --id Mozilla.Firefox --exact --silent --accept-package-agreements --accept-source-agreements
-winget install --id 9NKSQGP7F2NH -s msstore --silent --exact --accept-package-agreements --accept-source-agreements # whatsapp
-winget install --id RARLab.WinRAR --exact --silent --accept-package-agreements --accept-source-agreements
-winget install --id Anki.Anki --exact --silent --accept-package-agreements --accept-source-agreements
-winget install --id Telegram.TelegramDesktop --exact --silent --accept-package-agreements --accept-source-agreements
+# winget install --id RaveInc.Rave --exact --silent --accept-package-agreements --accept-source-agreements --ignore-security-hash --scope user
+# winget install --id BlueStack.BlueStacks --exact --silent --accept-package-agreements --accept-source-agreements --scope user
+# winget install --id Discord.Discord --exact --silent --accept-package-agreements --accept-source-agreements --scope user
+# winget install --id 9NKSQGP7F2NH -s msstore --silent --exact --accept-package-agreements --accept-source-agreements --scope user # whatsapp
+winget install --id Mozilla.Firefox --exact --silent --accept-package-agreements --accept-source-agreements --scope user
+winget install --id Anki.Anki --exact --silent --accept-package-agreements --accept-source-agreements --scope user
+winget install --id Telegram.TelegramDesktop --exact --silent --accept-package-agreements --accept-source-agreements --scope user
 
 winget uninstall --exact --id Microsoft.GameInput --silent --disable-interactivity --force
 winget uninstall --exact --id Microsoft.OneDrive --silent --disable-interactivity --force
